5848759f6f5ffa59fe50c9e530ebc7e7e54772b4
lilijang32782913 <504599497@qq.com>
2026-02-28 11:10:16 +0800

    1

---
+++ pretrain_weights/weights_config.sh
@@ -0,0 +1,3 @@
+# 训练脚本使用的权重路径配置（由 03_download_weights.sh 自动生成）
+export PRETRAIN_WEIGHTS="/root/AIWork/SmartCamera/PaddleDetection/PaddleDetection/pretrain_weights/ppyoloe_plus_crn_s_80e_coco.pdparams"
+export WEIGHTS_DIR="/root/AIWork/SmartCamera/PaddleDetection/PaddleDetection/pretrain_weights"
